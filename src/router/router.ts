b38c6fb454abaf47e27494c30e6e5f8617f6b5b1
sunruiguang <420007900@qq.com>
2024-03-18 14:01:42 +0800

    fix: add meta search ssr func

--- src/router/router.ts
+++ src/router/router.ts
@@ -8,5 +8,9 @@ const routers = [
     //采用路由懒加载的形式引入
     component: () => import("../views/WallpaperPage.vue"),
   },
+  {
+    path: "/GLg8YqDmDuP3QUSuqJUgu5gX4bGqg10w",
+    component: () => import("../views/WallpaperLevel.vue"),
+  },
 ];
 export default routers;
---
+++ src/views/WallpaperLevel.vue
@@ -0,0 +1,5 @@
+<template>
+  <div>// 在这里处理等级未0的壁纸，通过人为的给壁纸打上标签和对应的名称</div>
+</template>
+<script setup lang="ts">
+</script>
