957bd4915fc4b99d1642678a3a501a5b327ec32f
sunruiguang <420007900@qq.com>
2024-03-19 20:26:02 +0800

    fix: 手工升级壁纸信息

--- src/views/WallpaperLevel.vue
+++ src/views/WallpaperLevel.vue
@@ -27,13 +27,46 @@
       </el-table-column>
 
       <el-table-column fixed="right" label="操作">
-        <template #default>
-          <el-button link type="primary" size="small">编辑</el-button>
-          <el-button link type="danger" size="small">删除</el-button>
+        <template #default="scope">
+          <el-button
+            link
+            type="primary"
+            size="small"
+            @click="editRow(scope.row)"
+            >编辑</el-button
+          >
+          <el-button
+            link
+            type="danger"
+            size="small"
+            @click="deleteRow(scope.row)"
+            >删除</el-button
+          >
         </template>
       </el-table-column>
     </el-table>
   </div>
+  <el-dialog v-model="dialogFormVisible" title="Shipping address" width="500">
+    <el-form :model="form">
+      <el-form-item label="Promotion name" :label-width="formLabelWidth">
+        <el-input v-model="form.name" autocomplete="off" />
+      </el-form-item>
+      <el-form-item label="Zones" :label-width="formLabelWidth">
+        <el-select v-model="form.region" placeholder="Please select a zone">
+          <el-option label="Zone No.1" value="shanghai" />
+          <el-option label="Zone No.2" value="beijing" />
+        </el-select>
+      </el-form-item>
+    </el-form>
+    <template #footer>
+      <div class="dialog-footer">
+        <el-button @click="dialogFormVisible = false">Cancel</el-button>
+        <el-button type="primary" @click="dialogFormVisible = false">
+          Confirm
+        </el-button>
+      </div>
+    </template>
+  </el-dialog>
 </template>
 <script setup lang="ts">
 import { onMounted, reactive, ref } from "vue";
@@ -62,4 +95,17 @@ function loadWallpaper() {
       total.value = response.data.total;
     });
 }
+
+// 弹窗
+
+const dialogTableVisible = ref(false);
+const dialogFormVisible = ref(false);
+const formLabelWidth = "140px";
+
+const form = reactive({
+  name: "",
+});
+
+function editRow(row: any) {}
+function deleteRow(row: any) {}
 </script>
